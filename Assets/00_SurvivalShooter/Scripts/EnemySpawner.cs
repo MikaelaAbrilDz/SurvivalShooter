b5e7abd54a2095dfbe7c258c00f940da81d3fe67
MikaelaAbrilDz <mikaelaabrildiazvlc@gmail.com>
2026-05-25 19:51:54 +0200

    spawner enemigos creado

---
+++ Assets/00_SurvivalShooter/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject player;
+    [SerializeField] float spawnRange;
+
+    [SerializeField] float minFreq, maxFreq;
+    float timer = 0;
+    void Update()
+    {
+        if (timer <= 0)
+        {
+            timer = Random.Range(minFreq, maxFreq);
+            SpawnEnemy();
+        }
+        else timer -= Time.deltaTime;
+    }
+
+    void SpawnEnemy()
+    {
+        if (enemyPrefab == null) return;
+
+        float offset = Random.Range(0, spawnRange);
+
+        Vector3 offsetDirection = new Vector3(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f)).normalized;
+
+        GameObject lastEnemy = Instantiate(enemyPrefab, transform.position + offsetDirection * offset, Quaternion.identity);
+
+        lastEnemy.GetComponent<EnemyBehaviour>().target = player;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, spawnRange);
+    }
+}
